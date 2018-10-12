7eb88508c6219bd69e13bb19efd6496db0bc63ba
Noah <noah@karat.io>
2018-10-12 16:39:13 +0100

    Added PEP8 formatting, removed deprecated methods

--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # TODO #
 
-* Blank out non image type etc.
-* Use native dialog boxes
+* ~~Blank out non image type etc.~~
+* ~~Use native dialog boxes~~
 * Remove deprecated code
 * ~~Create git repo for project~~
 * Close without self.show()
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-    #!/usr/bin/python
-# -*- coding: utf-8 -*-
 import sys
 import subprocess
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, \
@@ -20,9 +18,8 @@ class App(QWidget):
 
     def saveFileDialog(self, window_title):
         options = QFileDialog.Options()
-        (fileName, _) = QFileDialog.getSaveFileName(self,
-                window_title, '',
-                'All Files (*);;Text Files (*.txt)', options=options)
+        (fileName, _) = QFileDialog.getSaveFileName(
+            self, window_title, '', 'MP4 File (*.mp4)', options=options)
         if fileName:
             print(fileName)
             return fileName
@@ -31,44 +28,48 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        image_filename = self.openFileNameDialog('Choose Image Filename', '(*.jpg *.jpeg *.png)')
+        image_filename = self.openFileNameDialog('Choose Image Filename',
+                                                 '(*.jpg *.jpeg *.png)')
         if image_filename is None:
             self.show()
             return
-        audio_filename = self.openFileNameDialog('Choose Audio Filename', '(*.mp3 *.wav)')
+
+        audio_filename = self.openFileNameDialog('Choose Audio Filename',
+                                                 '(*.mp3 *.wav)')
         if audio_filename is None:
             self.show()
             return
+
         save_file_destination = self.saveFileDialog('Name to save the file as')
-        print(save_file_destination)
+
         if save_file_destination is None:
             self.show()
             return
+
         self.fffmpeg_run(image_filename, audio_filename, save_file_destination)
         self.show()
 
     def openFileNameDialog(self,window_title,file_type):
         options = QFileDialog.Options()
 
-        (fileName, _) = QFileDialog.getOpenFileName(self,
-                window_title, '',
-                file_type, options=options)
+        (fileName, _) = QFileDialog.getOpenFileName(
+            self, window_title, '', file_type, options=options)
 
         if fileName:
             return fileName
 
-    def openFileNamesDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        (files, _) = QFileDialog.getOpenFileNames(self,
-                'QFileDialog.getOpenFileNames()', '',
-                'All Files (*);;Python Files (*.py)', options=options)
-        if files:
-            print(files)
-
-    def fffmpeg_run(self, image_filename, audio_filename, save_file_destination):
-        completed = subprocess.run("./fffmpeg -loop 1 -i " + '"' + image_filename + '"' + " -i " + '"' + audio_filename + '"' + " -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "+ '"' + save_file_destination + '"', shell=True)
+    def fffmpeg_run(self, image_filename, audio_filename,
+                    save_file_destination):
+        completed = subprocess.run("./fffmpeg -loop 1 -i " + '"'
+                                   + image_filename + '"' + " -i " + '"'
+                                   + audio_filename + '"' + " -c:v libx264"
+                                   " -tune stillimage -c:a aac -b:a 192k"
+                                   "-pix_fmt yuv420p -shortest "
+                                   + '"' + save_file_destination + '"',
+                                   shell=True)
         print('returncode:', completed.returncode)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
